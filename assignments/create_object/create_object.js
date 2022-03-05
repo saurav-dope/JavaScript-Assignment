3c6dbce62b52a420e22694611e4c3d2498bfce71
Saurav <sauravdutta1270@gmail.com>
2022-03-05 11:39:40 +0530

    Completed JavaScript Assignment-4

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,12 @@
 function CreateObject(arr) {
     // Write your code here
+    let obj = {};
+
+    for (let i = 0; i < arr.length; i+=2){
+            obj[arr[i]] = arr[i+1];
+    }
+
+    return obj;
 }
 
 module.exports = CreateObject;
